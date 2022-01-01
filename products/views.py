07f08bd90469440384c665ef340e158fb7cf930e
milton-kabir <milton.cse.pust@gmail.com>
2022-01-02 00:10:15 +0600

    "Done"

--- products/views.py
+++ products/views.py
@@ -1,11 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Product
+
 
 # Create your views here.
 
 # /products -> index
 def index(request):
-    return HttpResponse("Hello World")
+    products = Product.objects.all()
+    return render(request, "index.html", {"products": products})
+
 
 def newProducts(request):
     return HttpResponse("New Products have arrived")
