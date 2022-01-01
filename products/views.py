0a77567c486f467ed1a402517b6121d8099e1912
milton-kabir <milton.cse.pust@gmail.com>
2022-01-01 12:11:22 +0600

    Created Products Page

---
+++ products/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+# /products -> index
+def index(request):
+    return HttpResponse("Hello World")
+
